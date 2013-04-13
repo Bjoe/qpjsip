44b64e81c1370af00df92ca121d7cbff71458e9b
Joerg-Christian Boehme <joerg@chaosdorf.de>
2013-04-14 00:50:19 +0200

    Fix output.

--- tests/output.cpp
+++ tests/output.cpp
@@ -16,32 +16,37 @@ void Output::out(const qpjsua::PjError &anError, const QString &aMessage)
     if(anError.getStatus() != PJ_SUCCESS) {
         msg = QString("%1 Code: %2").arg(anError.getMessage()).arg(anError.getStatus()).toLatin1();
     }
-    std::cout << msg.constData() << std::endl;
+    std::cout << ">>> Output: " << msg.constData() << std::endl;
 }
 
 void Output::onLog(int level, QString message)
 {
-    std::cout << "Level " << level << " " << message.toLatin1().constData() << std::endl;
+    std::cout << ">>> Log >>> Level " << level << " >>> " << message.toLatin1().constData() << std::endl;
 }
 
 void Output::onIncomingCall(pjsua_acc_id accountId, pjsua_call_id callId)
 {
+    std::cout << ">>> onIncomingCall <<< " << std::endl;
     accountInfoOut(accountId);
     callInfoOut(callId);
 }
 
 void Output::onCallState(pjsua_call_id callId)
 {
-    callInfoOut(callId);}
+    std::cout << ">>> onCallState <<< " << std::endl;
+    callInfoOut(callId);
+}
 
 void Output::onCallMediaState(pjsua_call_id callId)
 {
+    std::cout << ">>> onCallMediaState <<< " << std::endl;
     callInfoOut(callId);
 }
 
 void Output::onRegStarted(pjsua_acc_id accountId, bool renew)
 {
     Q_UNUSED(renew);
+    std::cout << ">>> onRegStarted <<< " << std::endl;
     accountInfoOut(accountId);
 }
 
@@ -50,13 +55,33 @@ void Output::callInfoOut(pjsua_call_id callId)
     pjsua_call_info callInfo;
     pjsua_call_get_info(callId, &callInfo);
 
-    std::cout << "Local Info: " << callInfo.local_info.ptr << std::endl;
-    std::cout << "Local contact: " << callInfo.local_contact.ptr << std::endl;
-    std::cout << "Remote Info: " << callInfo.remote_info.ptr << std::endl;
-    std::cout << "Remote contact: " << callInfo.remote_contact.ptr << std::endl;
-    std::cout << "Media status: " << callInfo.media_status << std::endl;
-    std::cout << "State: " << callInfo.state << std::endl;
-    std::cout << "State Text: " << callInfo.state_text.ptr << std::endl;
+    const char *localInfo = pj_strbuf(&callInfo.local_info);
+    if(localInfo) {
+        std::cout << ">>> Local Info: " << localInfo << std::endl;
+    }
+
+    const char *localContact = pj_strbuf(&callInfo.local_contact);
+    if(localContact) {
+        std::cout << ">>> Local contact: " << localContact << std::endl;
+    }
+
+    const char *remoteInfo = pj_strbuf(&callInfo.remote_info);
+    if(remoteInfo) {
+        std::cout << ">>> Remote Info: " << remoteInfo << std::endl;
+    }
+
+    const char *remoteContact = pj_strbuf(&callInfo.remote_contact);
+    if(remoteContact) {
+        std::cout << ">>> Remote contact: " << remoteContact << std::endl;
+    }
+
+    const char *stateText = pj_strbuf(&callInfo.state_text);
+    if(stateText) {
+        std::cout << ">>> State Text: " << stateText << std::endl;
+    }
+
+    std::cout << ">>> Media status: " << callInfo.media_status << std::endl;
+    std::cout << ">>> State: " << callInfo.state << std::endl;
 }
 
 void Output::accountInfoOut(pjsua_acc_id accountId)
@@ -65,13 +90,18 @@ void Output::accountInfoOut(pjsua_acc_id accountId)
     pjsua_acc_get_info(accountId, &accountInfo);
 
     if(accountInfo.has_registration == PJ_TRUE) {
-        std::cout << "Client is registered";
+        std::cout << ">>> Client is registered" << std::endl;
     } else {
-        std::cout << "Client is not registered";
+        std::cout << ">>> Client is not registered" << std::endl;
     }
-    std::cout << "Status: " << accountInfo.status << std::endl;
-    std::cout << "Status Text: " << accountInfo.status_text.ptr << std::endl;
-    std::cout << "Reg last error: " << accountInfo.reg_last_err << std::endl;
+
+    const char *statusText = pj_strbuf(&accountInfo.status_text);
+    if(statusText) {
+        std::cout << ">>> Status Text: " << statusText << std::endl;
+    }
+
+    std::cout << ">>> Status: " << accountInfo.status << std::endl;
+    std::cout << ">>> Reg last error: " << accountInfo.reg_last_err << std::endl;
 }
 
 } // namespace tests
